e28617dd8440db90f3eeb711ba076105831102b7
muranjan <muktiranjanpatwa@gmail.com>
2020-06-02 02:21:41 +0530

    First Commit

---
+++ src/main/java/vy/app/VihangamYogaApplication.java
@@ -0,0 +1,19 @@
+package vy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class VihangamYogaApplication {
+  @PostConstruct
+  public void init(){
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(VihangamYogaApplication.class,args);
+  }
+}
