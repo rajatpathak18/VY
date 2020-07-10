9aa306faf7bcb57180c9d843258193041f663e5f
muranjan <muktiranjanpatwa@gmail.com>
2020-07-11 00:48:39 +0530

    jwt to accessToken

--- src/main/java/vy/app/pojo/AuthenticationResponse.java
+++ src/main/java/vy/app/pojo/AuthenticationResponse.java
@@ -9,15 +9,15 @@ import java.util.Collection;
 @Getter
 @Setter
 public class AuthenticationResponse {
-    private String jwt;
+    private String accessToken;
     private UserDto user;
     private Collection<? extends GrantedAuthority> authorities;
 
     AuthenticationResponse() {
     }
 
-    public AuthenticationResponse(String jwt, UserDto userDto, Collection<? extends GrantedAuthority> authorities) {
-        this.jwt = jwt;
+    public AuthenticationResponse(String accessToken, UserDto userDto, Collection<? extends GrantedAuthority> authorities) {
+        this.accessToken = accessToken;
         this.user = userDto;
         this.authorities = authorities;
     }
