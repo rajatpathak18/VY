3bc8862f5c8a977b0b4d2619c29b0785740daee4
Sunny Narayan <sunnykumar424@gmail.com>
2020-07-14 11:43:26 +0530

    fixed date timestamp milliseconds issue

--- src/main/java/vy/app/security/CorsConfigurer.java
+++ src/main/java/vy/app/security/CorsConfigurer.java
@@ -7,22 +7,21 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfigurer {
 
-  @Bean
-  public WebMvcConfigurer corsConfigure(){
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        System.out.println("cors configured ");
-        registry.addMapping("/**")
-          .allowedOrigins("*")
-          //.allowedHeaders("X-Requested-With", "Content-Type", "Authorization",
-           // "Origin", "Accept","HTTP2_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN","Access-Control-Allow-Origin","Referer")
-          .allowedHeaders("*")
-          .allowedMethods("GET","POST","PUT","OPTIONS");
-      }
-    };
-  }
+    @Bean
+    public WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                System.out.println("cors configured ");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        //.allowedHeaders("X-Requested-With", "Content-Type", "Authorization",
+                        // "Origin", "Accept","HTTP2_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN","Access-Control-Allow-Origin","Referer")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "OPTIONS");
+            }
+        };
+    }
 }
