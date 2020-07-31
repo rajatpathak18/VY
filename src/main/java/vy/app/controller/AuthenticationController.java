6e996d28a682784a05e4223b86a5fe98b0dbc75f
Sunny Narayan <sunnykumar424@gmail.com>
2020-07-31 22:21:23 +0530

    fixed auth error

--- src/main/java/vy/app/controller/AuthenticationController.java
+++ src/main/java/vy/app/controller/AuthenticationController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import vy.app.Exception.Exceptions;
 import vy.app.pojo.AuthenticationErrorResponse;
@@ -16,6 +17,8 @@ import vy.app.pojo.AuthenticationResponse;
 import vy.app.security.JwtUtils;
 import vy.app.security.VyUserDetails;
 import vy.app.util.UserConverter;
+
+import java.util.NoSuchElementException;
 
 @Log4j2
 @RestController
@@ -42,14 +45,9 @@ public class AuthenticationController {
             authenticationResponse = new AuthenticationResponse(jwt, converter.convertToDto(userDetails.getUser()), userDetails.getAuthorities());
 
             return new ResponseEntity<>(authenticationResponse, HttpStatus.OK);
-        } catch (BadCredentialsException e) {
-            System.out.println("Bad Credentials Exception " + e);
-            throw Exceptions.BadCredentialException;
-
         } catch (Exception e) {
-            log.info("Exceptions in authenticate");
-            log.info(e.getLocalizedMessage());
-            throw Exceptions.GeneralException;
+            System.out.println(" Exception in authentication" + e.getLocalizedMessage());
+            throw Exceptions.BadCredentialException;
         }
     }
 }
