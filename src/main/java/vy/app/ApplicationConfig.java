9502675c906841da44987358e91c65009ad797cf
Mukti Ranjan <Mukti@Muktis-MacBook-Pro.local>
2020-08-13 21:59:41 +0530

    adding datasource to run in cloud

--- src/main/java/vy/app/ApplicationConfig.java
+++ src/main/java/vy/app/ApplicationConfig.java
@@ -1,13 +1,26 @@
 package vy.app;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import vy.app.validation.MemberValidation;
 import vy.app.validation.UserValidation;
 
+import javax.sql.DataSource;
+
 @Configuration
+@PropertySource("Application.properties")
 public class ApplicationConfig {
+
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.url}")
+    private String url;
 
     @Bean
     public ModelMapper modelMapper() {
@@ -22,5 +35,15 @@ public class ApplicationConfig {
     @Bean
     public UserValidation userValidation() {
         return new UserValidation();
+    }
+
+    @Bean
+    public DataSource getDataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(this.url);
+        dataSourceBuilder.username(this.username);
+        dataSourceBuilder.password(this.password);
+        return dataSourceBuilder.build();
     }
 }
